ed3528dd35e58010c13b05131e0f6e1ccd72c81f
Jazzlyn <jasmin.mueller.ma@gmail.com>
2021-07-20 18:43:08 +0200

    Change: add dynamic permissions for filter

--- modules/filter/main.tf
+++ modules/filter/main.tf
@@ -8,15 +8,17 @@ terraform {
 }
 
 resource "jira_filter" "filter" {
-  for_each    = toset(var.filter.permissions)
   name        = var.filter.name
   jql         = var.filter.jql
   description = var.filter.description
   favourite   = var.filter.favourite
-  permissions {
-    type            = var.filter_permissions[each.value].type
-    group_name      = var.filter_permissions[each.value].group_name
-    project_id      = var.filter_permissions[each.value].project_id
-    project_role_id = var.filter_permissions[each.value].project_role_id
+  dynamic "permissions" {
+    for_each    = var.filter.permissions
+    content {
+      type            = var.filter_permissions[permissions.value].type
+      group_name      = var.filter_permissions[permissions.value].group_name
+      project_id      = var.filter_permissions[permissions.value].project_id
+      project_role_id = var.filter_permissions[permissions.value].project_role_id
+    }
   }
 }
